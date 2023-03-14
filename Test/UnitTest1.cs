71ecb3123a67cd7f6b103553e6ba57c8de7f2057
max <0922018@hr.nl>
2023-03-14 14:47:54 +0100

    Test Setup

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -3,9 +3,8 @@ namespace Test;
 [TestClass]
 public class UnitTest1
 {
- [TestMethod]
- public void TestMethod1()
- {
-  Program program = new(6);
- }
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
 }

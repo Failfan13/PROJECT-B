7dd5f983fd2322fcc1e52059511b9251fd2b526e
max <0922018@hr.nl>
2023-03-16 10:59:15 +0100

    Movie update

---
+++ Project/Logic/MoviesLogic.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+//This class is not static so later on we can use inheritance and interfaces
+class MoviesLogic
+{
+ private List<MovieModel> _movies;
+
+ //Static properties are shared across all instances of the class
+ //This can be used to get the current logged in account from anywhere in the program
+ //private set, so this can only be set by the class itself
+ static public MovieModel? CurrentAccount { get; private set; }
+
+ public MoviesLogic()
+ {
+  _movies = MoviesAccess.LoadAll();
+ }
+
+
+ public void UpdateList(MovieModel movie)
+ {
+  //Find if there is already an model with the same id
+  int index = _movies.FindIndex(s => s.Id == movie.Id);
+
+  if (index != -1)
+  {
+   //update existing model
+   _movies[index] = movie;
+  }
+  else
+  {
+   //add new model
+   _movies.Add(movie);
+  }
+  MoviesAccess.WriteAll(_movies);
+
+ }
+
+ public MovieModel GetById(int id)
+ {
+  return _movies.Find(i => i.Id == id);
+ }
+ public int GetNewestId(){
+    
+ }
+
+ public MovieModel NewMovie(string title, DateTime releaseDate, string director)
+ {
+
+  MovieModel movie = new MovieModel(, title, releaseDate, director);
+  UpdateList(movie);
+  return movie;
+ }
+}
+
+
+
+

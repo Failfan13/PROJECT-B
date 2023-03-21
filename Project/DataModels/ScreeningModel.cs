44a69391a853d68a7bfb61d6de0b28485f85edf7
Timtimthethird <125371174+Timtimthethird@users.noreply.github.com>
2023-03-21 15:28:12 +0100

    ScreeningModel + ScreeningData

---
+++ Project/DataModels/ScreeningModel.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+public class ScreeningModel
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("movieid")]
+    public int MovieId { get; set; }
+    [JsonPropertyName("screeningtime")]
+    public int ScreeningTime { get; set; }
+    [JsonPropertyName("screenid")]
+    public int ScreenId { get; set; }
+    [JsonPropertyName("chairs")]
+    public Dictionary<string, bool> Chairs{get; set;}
+    
+    public ScreeningModel(int id, int movieid, DateTime screeningTime, int screenId, Dictionary<string, bool> chairs)
+    {
+        Id = id;
+        MovieId = movieid;
+        ScreeningTime = screeningTime;
+        ScreenId = screenId;
+        Chairs = chairs;
+
+    }
+}

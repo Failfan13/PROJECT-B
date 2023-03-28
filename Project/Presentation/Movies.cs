3bed7042f200a292ea32b452a341ea74a2b74153
max <0922018@hr.nl>
2023-03-28 16:09:07 +0200

    Menu Done

--- Project/Presentation/Movies.cs
+++ Project/Presentation/Movies.cs
@@ -51,4 +51,50 @@ static class Movies
 
         Menu.Start();
     }
+
+    public static void ChangeMoviesMenu()
+    {
+        Console.Clear();
+        List<MovieModel> movies = MoviesLogic.AllMovies();
+        for (int i = 0; i < movies.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {movies[i].Title}");
+        }
+        Console.WriteLine($"{movies.Count + 1}. Return");
+        int awnser = QuestionLogic.AskNumber("Enter the number to select the movie");
+
+        if (awnser == movies.Count + 1)
+        {
+            Menu.Start();
+        }
+        ChangeMovieMenu(movies[awnser - 1]);
+
+    }
+
+    public static void ChangeMovieMenu(MovieModel movie)
+    {
+        Console.Clear();
+        Console.WriteLine("What would you like to do?");
+        List<string> options = new List<string>() { "Change title", "Change Director", "Change Date", "Change Description" };
+        for (int i = 0; i < options.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {options[i]}");
+        }
+        Console.WriteLine($"{options.Count + 1}. Return");
+
+        int awnser = QuestionLogic.AskNumber("");
+
+        if (awnser == 1)
+        {
+            // Change title
+        }
+        else if (awnser == 2)
+        {
+            // change director
+        }
+        else if (awnser == options.Count + 1)
+        {
+            ChangeMoviesMenu();
+        }
+    }
 }

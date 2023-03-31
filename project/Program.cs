fae8ab61c87db57cb28dfcb720b5de7bad821f11
max geurtsen <max@cris.nl>
2023-03-31 12:11:54 +0200

    ignore

--- project/Program.cs
+++ project/Program.cs
@@ -1,17 +1,1 @@
-﻿/*
-List<TimeSlotModel> slots = new List<TimeSlotModel>();
-
-for (int i = 0; i < 10; i++)
-{
-    slots.Add(new TimeSlotModel(i, i % 4, (DateTime.Now.AddDays(i)), new TheatherLogic().GetById(0)));
-}
-
-
-foreach (var item in slots)
-{
-    new TimeSlotsLogic().UpdateList(item);
-}
-*/
-
-
-Menu.Start();
+﻿Menu.Start();

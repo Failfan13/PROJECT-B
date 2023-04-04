fac3d79c84e8d06b1da59fb689ef81e94f6798ff
max <0922018@hr.nl>
2023-04-04 14:53:43 +0200

    try catch data type

--- Project/Logic/LogLogic.cs
+++ Project/Logic/LogLogic.cs
@@ -1,11 +1,27 @@
 static class Logger
 {
-    public static void Log(string LogString)
+    public static List<string> Types = new List<string>() { "Data Change", "User", "Reservation", "" };
+
+    public static void Log(string LogString, string type)
     {
-        string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
-        LogAccess.WriteLine($"{timestamp},{LogString}");
+        try
+        {
+
+            if (Types.Contains(type))
+            {
+                string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                LogAccess.WriteLine($"{timestamp},{type},{LogString}");
+            }
+            else
+            {
+                throw new Exception(searchStr + " is not present in the list.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
-
-
 }

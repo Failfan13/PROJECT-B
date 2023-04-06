340ba98f26a6893281b58ad8476389e2b54d3113
makayru <mmsb03@outlook.com>
2023-04-06 15:40:49 +0200

    Update Contact.cs

--- Project/Presentation/Contact.cs
+++ Project/Presentation/Contact.cs
@@ -4,6 +4,8 @@ public static class Contact
     {
         Console.Clear();
         Console.WriteLine("CONTACT INFO HERE\n");
+        Console.WriteLine("Call us at 123-456-7890 \nbetween 08:00 and 17:00\n");
+        Console.WriteLine("Or email us:\nCinema@hr.nl\n");
         Console.WriteLine("Press any key to return to the menu");
         Console.ReadLine();
         Menu.Start();

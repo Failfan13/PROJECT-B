2102162e91a53d7a1cc5f239908ac626c7f5a0a0
emircayir <89837128+emircayir@users.noreply.github.com>
2023-05-13 17:35:07 +0200

    Nuiance to Complaints

--- Project/Presentation/Admin.cs
+++ Project/Presentation/Admin.cs
@@ -20,7 +20,7 @@ public static class Admin
         Options.Add("Change Categories");
         Actions.Add(() => Category.Start());
 
-        Options.Add("Manage Complaints");
+        Options.Add("Change Complaints");
         Actions.Add(() => AccountsLogic.AdminSelectionComplaints());
 
         Options.Add("Change Reservations");

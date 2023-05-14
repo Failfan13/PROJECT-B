db4b242da86da11e322243528bc3a48d63a1cb0c
Erik Maan <failfan13gta5@gmail.com>
2023-05-14 13:47:06 +0200

    Bug fix Globilisation to en-US

--- project/Program.cs
+++ project/Program.cs
@@ -1,3 +1,8 @@
+using System.Globalization;
+// Set system US Culture
+System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("en-US");
+System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+// Show unicode characters
 Console.OutputEncoding = System.Text.Encoding.Unicode;
 
 Menu.Start();
